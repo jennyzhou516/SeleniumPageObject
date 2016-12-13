884c353fcbc0f70a936503a3f2a64bdf04a3f22b
huyphan <dinhhuy131@gmail.com>
2016-12-13 23:23:34 +0700

    Update pageobject

--- src/pages/base/PageObject.java
+++ src/pages/base/PageObject.java
@@ -1,5 +1,11 @@
 package pages.base;
 
-public class PageObject {
+import org.openqa.selenium.WebDriver;
 
+public class PageObject {
+	protected WebDriver driver;
+	
+	public PageObject(WebDriver driver){
+		this.driver = driver;
+	}
 }
