642e551a1191e343801e6be7a89a2c270ce9ba62
huyphan <dinhhuy131@gmail.com>
2016-12-28 07:05:02 +0700

    Update data test and define elements for signup page

--- src/page/object/SignUp.java
+++ src/page/object/SignUp.java
@@ -1,5 +1,7 @@
 package page.object;
 
+import java.util.HashMap;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -15,6 +17,71 @@ public class SignUp extends PageObject {
 	
 	@FindBy(id="lang-chooser")
 	public WebElement langChooser;
+	
+	@FindBy(id="FirstName")
+	public WebElement firstName;
+	
+	@FindBy(id="LastName")
+	public WebElement lastName;
+	
+	@FindBy(id="GmailAddress")
+	public WebElement gmailAddress;
+	
+	@FindBy(id="Passwd")
+	public WebElement passwd;
+	
+	@FindBy(id="PasswdAgain")
+	public WebElement passwdAgain;
+	
+	@FindBy(xpath=".//*[@id='BirthMonth']/div")
+	public WebElement birthMonth;
+	
+	@FindBy(id="BirthDay")
+	public WebElement birthDay;
+	
+	@FindBy(id="BirthYear")
+	public WebElement birthYear;
+	
+	@FindBy(xpath=".//*[@id='Gender']/div")
+	public WebElement gender;
+	
+	@FindBy(id="RecoveryPhoneNumber")
+	public WebElement recoveryPhoneNumber;
+	
+	@FindBy(xpath=".//*[@id='CountryCode']/div")
+	public WebElement countryCode;
+	
+	@FindBy(id="errormsg_0_FirstName")
+	public WebElement errormsg_FirstName;
+	
+	@FindBy(id="errormsg_0_LastName")
+	public WebElement errormsg_LastName;
+	
+	@FindBy(id="errormsg_0_GmailAddress")
+	public WebElement errormsg_GmailAddress;
+	
+	@FindBy(id="errormsg_0_Passwd")
+	public WebElement errormsg_Passwd;
+	
+	@FindBy(id="errormsg_0_PasswdAgain")
+	public WebElement errormsg_PasswdAgain;
+	
+	@FindBy(id="errormsg_0_BirthMonth")
+	public WebElement errormsg_BirthMonth;
+	
+	@FindBy(id="errormsg_0_BirthDay")
+	public WebElement errormsg_BirthDay;
+	
+	@FindBy(id="errormsg_0_BirthYear")
+	public WebElement errormsg_BirthYear;
+	
+	@FindBy(id="errormsg_0_Genderr")
+	public WebElement errormsg_Gender;
+	
+	@FindBy(id="submitbutton")
+	public WebElement nextStep;
+	
+	
 
 
 	public SignUp(WebDriver driver){
@@ -27,6 +94,10 @@ public class SignUp extends PageObject {
 		Select lang_select = new Select(langChooser);
 		lang_select.selectByValue("en");
 		sleep(5);
+		
+	}
+	
+	public void inputData(HashMap<String,String> fieldData){
 		
 	}
 }
